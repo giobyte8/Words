6a799b1cabe6da224f3351874aaf986a862eb813
Giovanni Aguirre <gaguirre@skytouchtechnology.com>
2021-11-06 20:40:37 -0600

    Script to build app through docker

---
+++ docker/build_app.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Compiles app into executable jar using a docker
+# gradle container
+#
+
+# ref: https://stackoverflow.com/a/4774063/3211029
+HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd "${HERE}/.." || return
+
+APP_ROOT=$(pwd)
+cd "${HERE}" || return
+
+echo
+echo "Building app from ${APP_ROOT}"
+
+docker run --rm -u gradle \
+  -v "$APP_ROOT":/home/gradle/project \
+  -w /home/gradle/project \
+  gradle:7.2.0-jdk17-alpine gradle build
